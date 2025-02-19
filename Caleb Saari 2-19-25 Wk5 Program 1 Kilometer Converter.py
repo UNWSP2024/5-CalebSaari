dd2368ce629a93a3eec565eae8373f37eae53dfe
CalebSaari <saaricaleb32@gmail.com>
2025-02-19 14:50:58 -0600

    Caleb Saari 2/19/25 Wk5 Programs

---
+++ Caleb Saari 2-19-25 Wk5 Program 1 Kilometer Converter.py
@@ -0,0 +1,14 @@
+def kilometer_conversion(kilometers):
+    miles = 0.0
+    miles = kilometers*0.6214
+
+    return miles
+
+
+
+if __name__ == '__main__':
+    print("Kilometer Converter")
+    kilometers = float(input("How many kilometers would you like to convert?:"))
+miles = kilometer_conversion(kilometers)
+
+print(kilometers,"kilometers is equal to",miles, "miles")
